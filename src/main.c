232f36fdb3694ad3c178f72bb95bc4646ff3f9f9
aksh_raz <sur37704@gmail.com>
2025-03-27 18:57:16 +0530

    reading wav

--- src/main.c
+++ src/main.c
@@ -22,9 +22,34 @@ typedef struct {
     // data Subchunk
     char subchunk2id[4];
     int32_t subchunk2size;
-} header;
+} WAVHeader;
+
+void read_wav_header(const char* filename) {
+    FILE* file = fopen(filename, "rb");
+    if (!file) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    WAVHeader header;
+    fread(&header, sizeof(WAVHeader), 1, file);
+    
+    if (strncmp(header.chunkid, "RIFF", 4) != 0 || strncmp(header.format, "WAVE", 4) != 0) {
+        printf("Not a valid WAV file!\n");
+        fclose(file);
+        return;
+    }
+
+    printf("Audio Format: %d\n", header.format);
+    printf("Channels: %d\n", header.numchannels);
+    printf("Sample Rate: %d Hz\n", header.samplerate);
+    printf("Bit Depth: %d-bit\n", header.bitspersample);
+    printf("Data Size: %u bytes\n", header.subchunk2size);
+
+    fclose(file);
+}
 
 int main() {
-    FILE *fp;
-    header h;
+    read_wav_header("sample.wav");
+    return 0;
 }
