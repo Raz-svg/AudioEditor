0bbb22def48bde9db5bf94974a74866d380cf9e3
aksh_raz <sur37704@gmail.com>
2025-03-27 12:54:30 +0530

    wav header

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct{
+
+    //riff header
+    char chunkid[4]="RIFF";
+    int chunksize;
+    char format[4]="WAVE";
+    //fmt(format) subchunk
+    char subchunk1id[4]="fmt ";
+    int subchunk1size=16;
+    //data subchunk
+    char subchunk2id[4]="data";
+    int subchunk2size;
+    
+}header;
+
+int main(){
+
+}
