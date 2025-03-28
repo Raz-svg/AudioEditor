e20993005495ec259e2003e6457bdab704d291cd
aksh_raz <sur37704@gmail.com>
2025-03-28 09:39:17 +0530

    reading pcm data

--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@ typedef struct {
     char chunkid[4];
     int32_t chunksize;
     char format[4];
-
     // fmt Subchunk
     char subchunk1id[4];
     int32_t subchunk1size;
@@ -18,12 +17,13 @@ typedef struct {
     int32_t byterate;
     int16_t blockalign;
     int16_t bitspersample;
-
     // data Subchunk
     char subchunk2id[4];
-    int32_t subchunk2size;
+    int32_t subchunk2size; // actual audio data size
 
 } WAVHeader;
+
+WAVHeader header;
 
 
 // mainly for debugging
@@ -34,7 +34,6 @@ void read_wav_header(const char* filename) {
         return;
     }
 
-    WAVHeader header;
     fread(&header, sizeof(WAVHeader), 1, file);
     
     if (strncmp(header.chunkid, "RIFF", 4) != 0 || strncmp(header.format, "WAVE", 4) != 0) {
@@ -49,13 +48,34 @@ void read_wav_header(const char* filename) {
     printf("Bit Depth: %d-bit\n", header.bitspersample);
     printf("Data Size: %u bytes\n", header.chunksize);
 
-    fclose(file);i
+    fclose(file);
+}
+
+// reading raw pcm data
+int16_t *read_pcm_data(const char* filename) {
+    FILE* file = fopen(filename, "rb");
+
+    // moves the pointer offset bytes from the origin
+    fseek(file, 44, SEEK_SET);// skip the header i.e 44 bytes
+
+    // read the data
+    int16_t* data = (int16_t*)malloc(header.subchunk2size);
+    fread(data, header.subchunk2size, 1, file);
+
+    // do something with the data
+    fclose(file);
+    return data;
 }
 
 
-
-
 int main() {
+    
     read_wav_header("sample.wav");
+    int16_t* v=read_pcm_data("sample.wav");
+    for(int i=0;i<100;i++)
+    {
+        printf("%d\n",*(v+i));
+    }
+    
     return 0;
 }
