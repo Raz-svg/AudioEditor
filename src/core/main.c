8bc65c04e0776c11b19aac1c9ca94d43eda4506b
aksh_raz <sur37704@gmail.com>
2025-03-31 18:03:55 +0530

    mouse click

--- src/core/main.c
+++ src/core/main.c
@@ -10,21 +10,22 @@ void draw_waveform(SDL_Renderer *renderer, int16_t *pcm, int num_samples) {
     int scale = 100; // Scaling factor for better visibility
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    for (int i = 0; i < num_samples ; i++) {
+    SDL_RenderDrawLine(renderer,0,centerY,SCREEN_WIDTH,centerY);
+    for (int i = 0; i < num_samples; i++) {
         int x1 = i * SCREEN_WIDTH / num_samples;
         int y1 = centerY - (pcm[i] / scale);
-        int x2 = i * SCREEN_WIDTH / num_samples;
+        int x2 = x1;
         int y2 = centerY;
-        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2); 
+        SDL_RenderDrawPoint(renderer, x1, y1);
+
     }
+
 }
 
-
-int factor=4;
-// reduce the number of samples rendered that would generate a zoom effect  so ..reduce num_samples by a factor every time zoom is called  in a particular area
-void zoom(int* num_samples){
-
-	*num_samples=(*num_samples)/factor;
+int factor = 2;
+void zoom(int *num_samples) {
+    *num_samples = (*num_samples) / factor;
 }
 
 int main() {
@@ -44,24 +45,26 @@ int main() {
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Waveform Visualization",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Waveform Visualization", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     int running = 1;
     SDL_Event event;
+    int samples = header.subchunk2size / sizeof(int16_t);
 
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
                 running = 0;
+            
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                zoom(&samples);
+            }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         
-	int samples=header.subchunk2size/sizeof(int16_t);
-        // Draw waveform for the first file
-	zoom(&samples);
         if (pcm1) draw_waveform(renderer, pcm1, samples);
 
         SDL_RenderPresent(renderer);
@@ -74,3 +77,4 @@ int main() {
     free_list();
     return 0;
 }
+
